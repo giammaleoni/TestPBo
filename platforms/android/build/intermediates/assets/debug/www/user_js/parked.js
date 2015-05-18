41016938a1f9d4b71358dc10f1840acb92593443
giammaleoni <gianmaria.leoni@gmail.com>
2015-05-18 10:38:12 +0200

    Esclusa cartella /build da .gitignore

---
+++ platforms/android/build/intermediates/assets/debug/www/user_js/parked.js
@@ -0,0 +1,241 @@
+// constant declatration
+// constant declatration
+var X = "X";
+var monthNames = [
+        "Gennaio", "Febbraio", "Marzo",
+        "Aprile", "Maggio", "Giugno", "Luglio",
+        "Agosto", "Settembre", "Ottobre",
+        "Novembre", "Dicembre"
+    ];
+
+//***********************************************
+//Giorno e settimana
+// converte il primo numero passato nel progressivo
+// ed il secondo nel giorno della settimana
+//***********************************************
+getWeekDay = function(week,day){
+	switch(week) {
+        case 1:
+            var week_string ="1°";
+            break;
+        case 2:
+            var week_string ="2°";
+            break;
+        case 3:
+            var week_string ="3°";
+            break;
+        case 4:
+            var week_string ="4°";
+            break;
+        case 5:
+            var week_string ="5°";
+            break;
+	};
+
+	switch(day) {
+        case 1:
+            var day_string ="lunedì";
+            break;
+        case 2:
+            var day_string ="martedì";
+            break;
+        case 3:
+            var day_string ="mercoledì";
+            break;
+        case 4:
+            var day_string ="giovedì";
+            break;
+        case 5:
+            var day_string ="venerdì";
+            break;
+    	case 6:
+    	    var day_string ="sabato";
+   	     	break;
+  	  	case 7:
+        	var day_string ="domenica";
+        	break;
+	};
+
+	return [week_string, day_string];
+
+};
+
+//***********************************************
+// Elimina il parcheggio dalla local storage
+//***********************************************
+sparcheggia = function(){ 
+
+	if (localStorage.parcheggio == null){
+		infoMsg("La macchina non era parcheggiata.");
+	}else{
+		localStorage.removeItem("parcheggio");
+		if (localStorage.parcheggio == null){
+			infoMsg("Hai appena sparcheggiato la macchina");
+			parkAttuale();
+		};
+	};	
+};
+
+
+//***********************************************
+//	Passati il giorno della week e il counter restituisce l'elenco 
+//	dey giorni per 12 mesi in un array ("days[]")
+//***********************************************
+getDays12Months = function(n_giorno, giorno){
+
+//    var n_giorno = 4;				//counter del giorno nel mese	--> 4°
+//    var giorno = 3;				//giorno della week 			--> mercoledì
+
+    var d;
+    var today =  new Date();
+    var month = today.getMonth();
+    var year = today.getFullYear();
+    var day = today.getDate();
+    var count;
+    var days = [];	
+
+
+    for(i=0; i<12; i++){
+    	//d = new Date(today.getFullYear(),month,today.getDate());
+		//d.setDate(1);
+		d = new Date(today.getFullYear(),month)
+    	month++;
+
+    
+        // Get the first "giorno" of the month
+        while (d.getDay() !== giorno) {
+            d.setDate(d.getDate() + 1);
+        }
+    
+ 	   // Get the n_giorno° "giorno of the month
+ 	   count = 1;
+		while (count <= n_giorno){
+			days[i] = new Date(d.getTime());
+ 	       d.setDate(d.getDate() + 7);
+ 	       count = count + 1;
+ 	   }
+	}
+
+//    alert(days.join('\n'));
+    return(days);
+};
+
+//***********************************************
+//	restituisce le date per cui si deve pianificare il job
+//	per la via in memoria
+//***********************************************
+getDays12MonthByAddress = function(NoAlert){
+	
+	var via = localStorage.parcheggio;
+	
+	if (via != null){
+		var n_g = matrixLavaggio.vlookup(via,1);
+		var g = matrixLavaggio.vlookup(via,2);
+		
+		if (n_g != null && g != null){
+			var giorniJob = getDays12Months(n_g,g);
+			var weekDay_string = getWeekDay(n_g,g);
+			var n_g_string = weekDay_string[0];
+			var g_string = weekDay_string[1];
+		
+			if (NoAlert != "X"){
+				//alert(giorniJob.join("\n"));
+				return (giorniJob);
+			}else{
+				var giorniJobForm = [];
+				for (j=0;j<12;j++){
+					var monthIndex = giorniJob[j].getMonth();
+					giorniJobForm[j] = giorniJob[j].getDate() + " " + monthNames[monthIndex] + " " + giorniJob[j].getFullYear() + " 00:00";
+				};
+				document.getElementById("listaLavaggio").innerHTML = "In " + via + " il lavaggio strade è previsto il "+ n_g_string + " " + g_string+ " del mese <hr /><br />" + giorniJobForm.join("<br />");
+			}
+		}else{
+			if (NoAlert != "X"){
+				infoMsg("Via non presente in anagrafica");
+				//evita l'errore in case di cambio elenco vie
+				localStorage.removeItem('parcheggio');
+			}else{
+				document.getElementById("listaLavaggio").innerHTML = null;
+				infoMsg("Via non presente in anagrafica");
+			};
+		}	
+	}else{
+		if (NoAlert != "X"){
+			infoMsg("Auto non parcheggiata");
+		}else{
+			document.getElementById("listaLavaggio").innerHTML = null;
+			infoMsg("Auto non parcheggiata");
+		};
+	};
+};
+
+//***********************************************
+//	Scrive la via in cui è parcheggiata l'auto nella main page
+//	
+//***********************************************
+parkAttuale = function(){
+	var parcheggio = localStorage.parcheggio;
+	if (parcheggio == null || parcheggio == ""){
+		document.getElementById("park_id").innerHTML = "L'auto non è parcheggiata";
+		document.getElementById("park_id2").innerHTML = "";
+	}else{
+		document.getElementById("park_id").innerHTML = "L'auto è attualmente parcheggiata in <br><u>" + localStorage.parcheggio +"</u>";
+		//recupero il primo giorno del lavaggio o uguale alla data odierna
+		var arrayGiorni = getDays12MonthByAddress();
+		var today = new Date();
+		var settings = JSON.parse(localStorage["settings"]);
+		var memo1 = settings[2];
+		var diff;
+		for (i=0;i<12;i++){
+			//orario limite per considerare il lavaggio concluso --> 06:00:00
+			arrayGiorni[i].setHours(6,0,0,0);
+			if(arrayGiorni[i] >= today) {
+				//1000 milli secondi * 60 secondi * 60 minuti * 24 ore
+				diff = Math.round((arrayGiorni[i] - today)/(1000*60*60*24));
+				var month = arrayGiorni[i].getMonth();
+    			var year = arrayGiorni[i].getFullYear();
+    			var day = arrayGiorni[i].getDate();
+    			var day7 = arrayGiorni[i].getDay();
+    			var weekDay = getWeekDay(null,day7);
+    			// se la differenza è inferiore o uguale al memo1 evidenzio in rosso
+				if (diff <= memo1){
+					$("#park_id2").addClass("red");
+					document.getElementById("park_id2").innerHTML = "ATTENZIONE!! <br> Prossimo lavaggio: " + weekDay[1] + " " + day + " " + monthNames[month] + " " + year;
+				}else{
+    				$("#park_id2").removeClass("red");
+					document.getElementById("park_id2").innerHTML = "Prossimo lavaggio: " + weekDay[1] + " " + day + " " + monthNames[month] + " " + year;
+				}
+				break;
+			}
+		}
+	};
+}
+
+//***********************************************
+//	Parcheggia con dropdownlist
+//	
+//***********************************************
+parcheggiaDD = function(){
+	var via = document.getElementById("id_via").value;
+	//funzione che esegue il parcheggio
+	park(via);
+}
+
+//***********************************************
+//	Funzione che contiene tutti gli step del parcheggio
+//		viene richiamata dalla dropdown list o dalla geolocalizzazione
+//***********************************************
+park = function(indirizzo){
+	
+//	var check1 = matrixLavaggio.vlookup(indirizzo,1);
+//	var check2 = matrixLavaggio.vlookup(indirizzo,2);
+		
+//		if (check1 != null && check2 != null){
+    		localStorage.parcheggio = indirizzo;
+        	infoMsg("Hai parcheggiato in " + localStorage.parcheggio);
+    		parkAttuale();
+//    	}else{
+//    		infoMsg(Indirizzo + " non presente in anagrafica!");
+//    	}
+}
+
