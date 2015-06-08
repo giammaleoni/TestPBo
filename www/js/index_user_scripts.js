deb0325a393d745c7aac2ca924aa9d77d5fa9290
Gianmaria Leoni <gianmaria.leoni@gmail.com>
2015-06-08 17:16:37 +0200

    mi ero dimenticato un file...

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -10,35 +10,6 @@
  {	
 	//nascondo la maschera di caricamento quando l'app è stata caricata
 	$.ui.hideMask()
-	
-//*********************************************************************************************************
-//*********************************************************************************************************
-//Inizializzazione mappa all'avvio:
-//*********************************************************************************************************
-	caricaMappa = function(){
-	console.log("Inizio caricamento MAPPA") ;
-	var options = {
-			//frequency: 5000,
-			maximumAge: 0,				//il sistema accetta posizioni non più vecchie di 0 millisecondi
-			timeout: 10000,				//timeout error dopo 10 sec
-			enableHighAccuracy: true,	//posizione accurata
-		};
-
-	// AFTER the deviceready event:
-	if(app.geolocation) {
-		var locationService = app.geolocation; // native HTML5 geolocation
-	}
-	else {
-		var locationService = navigator.geolocation; // cordova geolocation plugin
-	}
-	//locationService.getCurrentPosition(app.onSuccess, app.onError, options);		
-	id = locationService.watchPosition(app.onSuccess, app.onError, options);
-	};
-	
-	//window.setTimeout(caricaMappa, 0);
-
-//*********************************************************************************************************
-//*********************************************************************************************************	
 
 //*********************************************************
 //		ONCLICK events
