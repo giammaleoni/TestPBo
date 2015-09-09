6fea38755a26c95f0e80ef85dd31bf57a41aea60
maxvaega <mambozzo@gmail.com>
2015-09-10 00:29:12 +0200

    Revert "Matrice lavaggio v0.5"
    This reverts commit ed57afe4adf0799df4c3a610a0df9e979398c116.

--- www/user_js/updateMatrixLavaggio.js
+++ www/user_js/updateMatrixLavaggio.js
@@ -2,7 +2,7 @@
 // 		se la connessione è disponibile lo script si carica e aggiorna i dati sulla local storage che contiene
 //		tutti i dati di tutte le vie di Bologna.
 
-var versioneMatrice = '0.5';
+var versioneMatrice = '0.4';
 var versioneOld = localStorage.matriceVersione;
 
 
@@ -25,17 +25,6 @@ if (versioneOld != versioneMatrice) {
 {id:12,	idHera:731,	viaHera:"ARIENTI (VIA)",	dettaglioHera:null,	viaGoogle:"Via Arienti",	minDisp:null,	minPari:null,	maxDisp:null,	maxPari:null,	day:5,	week:4},
 {id:13,	idHera:741,	viaHera:"ARTIERI (VIA)",	dettaglioHera:null,	viaGoogle:"Via degli Artieri",	minDisp:null,	minPari:null,	maxDisp:null,	maxPari:null,	day:5,	week:2},
 {id:14,	idHera:566,	viaHera:"AVESELLA (VIA)",	dettaglioHera:null,	viaGoogle:"Via Avesella",	minDisp:null,	minPari:null,	maxDisp:null,	maxPari:null,	day:3,	week:1},
-		
-{id:15,	idHera:526,	viaHera:"AZZARITA (P.ZZA)",	dettaglioHera:"da via Calori a via Grimaldi",	viaGoogle:"Piazza Manfredi Azzarita",	minDisp:5,	minPari:4,	maxDisp:5,	maxPari:6,	day:2,	week:2},
-{id:16,	idHera:526,	viaHera:"AZZARITA (P.ZZA)",	dettaglioHera:"da via Calori a via Riva Reno",	viaGoogle:"Piazza Manfredi Azzarita",	minDisp:1,	minPari:8,	maxDisp:3,	maxPari:8,	day:2,	week:2},
-{id:17,	idHera:500,	viaHera:"AZZO GARDINO (VIA)",	dettaglioHera:"da via Riva Reno a L.go c.ti del lavoro",	viaGoogle:"Via Azzo Gardino",	minDisp:1,	minPari:2,	maxDisp:3,	maxPari:10,	day:2,	week:1},
-{id:18,	idHera:500,	viaHera:"AZZO GARDINO (VIA)",	dettaglioHera:"da via Castellaccio a via del Rondone ambo i lati",	viaGoogle:"Via Azzo Gardino",	minDisp:23,	minPari:0,	maxDisp:23,	maxPari:51,	day:2,	week:2},
-{id:19,	idHera:500,	viaHera:"AZZO GARDINO (VIA)",	dettaglioHera:"da via Menarini a via Del Rondone",	viaGoogle:"Via Azzo Gardino",	minDisp:55,	minPari:38,	maxDisp:65,	maxPari:40,	day:3,	week:4},
-{id:20,	idHera:627,	viaHera:"BARACCANO (P.ZZA)",	dettaglioHera:null,	viaGoogle:"Piazza del Baraccano",	minDisp:null,	minPari:null,	maxDisp:null,	maxPari:null,	day:3,	week:4},
-{id:21,	idHera:721,	viaHera:"BARBAZZI (VIA)",	dettaglioHera:null,	viaGoogle:"Vicolo Barbazzi",	minDisp:null,	minPari:null,	maxDisp:null,	maxPari:null,	day:5,	week:4},
-{id:22,	idHera:517,	viaHera:"BARBERIA (VIA)",	dettaglioHera:null,	viaGoogle:"Via Barberia",	minDisp:null,	minPari:null,	maxDisp:null,	maxPari:null,	day:2,	week:2},
-
-
 							];
 
 	localStorage.matrixLavaggioLocal = JSON.stringify(matrixLavaggioNew);
